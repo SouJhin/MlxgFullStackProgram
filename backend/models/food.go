adf90ea538836a7799291a7bc6c5c1e8917c7fed
SouJhin <soujhin2022@gmail.com>
2023-02-24 13:15:35 +0800

    such git global

--- backend/models/food.go
+++ backend/models/food.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Food struct {
-	Image      string    `xorm:"image" json:"image,omitempty"`
-	Name       string    `json:"name,omitempty"`
-	ID         string    `json:"id,omitempty"`
-	Price      int       `json:"price,omitempty"`
-	Describe   string    `json:"describe,omitempty"`
-	CreaatedAt time.Time `xorm:"created" json:"creaated_at"`
+	Image     string    `xorm:"image" json:"image,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	ID        string    `json:"id,omitempty"`
+	Price     int       `json:"price,omitempty"`
+	Describe  string    `json:"describe,omitempty"`
+	CreatedAt time.Time `xorm:"created" json:"created_at"`
 }
